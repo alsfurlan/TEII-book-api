03e13500cf3aa48dcdd0241072bc15aceb513505
Anderson Luis Furlan <alsfurlan@gmail.com>
2020-06-09 21:17:33 -0300

    Criado CRUD em memória de autores

---
+++ src/main/java/br/com/bookapi/models/Autor.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bookapi.models;
+
+import java.util.UUID;
+
+/**
+ *
+ * @author aula
+ */
+public class Autor {
+    
+    private UUID id;
+    private String nome;
+
+    public Autor() {
+    }
+    
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
